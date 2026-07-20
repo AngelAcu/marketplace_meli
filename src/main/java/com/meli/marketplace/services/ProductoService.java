f259ce720eda031d844663f2b2327c8c8a9b4ce7
shadowsouls444 <angel.acuna.meza@gmail.com>
2026-07-20 13:29:50 -0500

    refactor: se agrego la anotacion @Transactional en el metodo save por la clase ProductoService

--- src/main/java/com/meli/marketplace/services/ProductoService.java
+++ src/main/java/com/meli/marketplace/services/ProductoService.java
@@ -10,6 +10,7 @@ import com.meli.marketplace.repositories.ProductoRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -49,6 +50,7 @@ public class ProductoService {
 
     }
 
+    @Transactional
     public ProductoResponseDTO save(ProductoRequestDTO productoRequestDto){
 
         Producto producto = new Producto();
